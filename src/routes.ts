75f90c78aa07159af5cc953c3047ad2f385fd0e4
Jonas <jonas.aquinos@hotmail.com>
2023-08-01 17:54:44 -0300

    Criando controller e services antes de conectar a aplicação com BD

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,9 @@
-import { Router, Request, Response } from "express";
+import { Router } from "express";
+import { CreateUserController } from "./controllers/User/CreateUserController";
 
 const router = Router();
 
-router.get('/teste', (req: Request, res: Response) => {
-    return res.json({ nome: 'John' });
-})
+//ROTAS USER
+router.post('/users', new CreateUserController().handle)
 
 export { router }
