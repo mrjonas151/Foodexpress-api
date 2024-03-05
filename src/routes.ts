2dab54e1362b33a6c553b7d44a06e6bf2830aabc
mrjonas151 <jonas.aquinos@hotmail.com>
2024-03-05 18:47:39 -0300

    Terminando sistema de autenticação de usuário e cadastro de categoria

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,20 @@
 import { Router } from "express";
 import { CreateUserController } from "./controllers/User/CreateUserController";
 import { AuthUserController } from "./controllers/User/AuthUserController";
+import { DetailUserController } from "./controllers/User/DetailUserController";
+import { isAuthenticated } from "./middlewares/isAuthenticated";
+import { CreateCategoryController } from "./controllers/Category/CreateCategoryController";
 const router = Router();
 
 //ROTAS USER
 router.post('/users', new CreateUserController().handle)
 
 router.post('/session', new AuthUserController().handle) //rota de login
+
+router.get('/me', isAuthenticated, new DetailUserController().handle)
+
+
+//ROTAS CATEGORY
+router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 
 export { router }
