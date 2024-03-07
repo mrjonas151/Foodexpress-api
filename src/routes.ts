36c20beccf37d3d13481e31f5ea0c3c38ae69159
mrjonas151 <jonas.aquinos@hotmail.com>
2024-03-07 18:59:51 -0300

    Criando cadastro de produto e adicionando imagens ao banco de dados da aplicação

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
+import multer from "multer";
 import { CreateUserController } from "./controllers/User/CreateUserController";
 import { AuthUserController } from "./controllers/User/AuthUserController";
 import { DetailUserController } from "./controllers/User/DetailUserController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 import { CreateCategoryController } from "./controllers/Category/CreateCategoryController";
+import { ListCategoryController } from "./controllers/Category/ListCategoryController";
+import { CreateProductController } from "./controllers/Product/CreateProductController"; 
+import uploadConfig from "./config/multer"
+
 const router = Router();
+
+const upload = multer(uploadConfig.upload("./tmp"));
 
 //ROTAS USER
 router.post('/users', new CreateUserController().handle)
@@ -16,5 +23,14 @@ router.get('/me', isAuthenticated, new DetailUserController().handle)
 
 //ROTAS CATEGORY
 router.post('/category', isAuthenticated, new CreateCategoryController().handle)
+
+router.get('/category', isAuthenticated, new ListCategoryController().handle)
+
+
+//ROTAS PRODUCT
+router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
+
+
+
 
 export { router }
